8af19d7121bd356c05016bcf551e2dd4d79d6ea5
Cloud User <centos@ip-172-31-33-213.us-west-2.compute.internal>
2017-02-09 22:31:10 +0000

    Chef Resources

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,11 +8,47 @@ package 'httpd' do
     action :install
 end
 
+#remote_file '/var/www/html/peacock.png' do
+#    source 'http://www.chrisjanson.com/sites/g/files/g2000005281/f/sample_01.jpg'
+#end
+
+#cookbook_file '/var/www/html/index.html' do
+#    source 'index.html'
+#end
+
 template 'var/www/html/index.html' do
     source 'index.html.erb'
-    action :create
+#:before, :delayed, :immediately
+#
+#notifies :action, 'resource[name]', :timer
+#subscribes :aciton, 'resource[name]', :timer
+#    notifies :restart, 'service[httpd]', :immediately
 end
+
+#bash "Some Script" do
+#    user "root"
+#    code "mkdir -p /var/www/mysites/ && chown -R apache /var/www/mysites/"
+   # not_if '[-d /var/www/mysites/]'
+#    not_if do
+#	File.directory?('/var/www/mysites/')
+#    end
+   #only_if
+#end
+
+#execute "run a script" do
+#    user "root"
+#    command <<-EOH
+#        mkdir -p /var/www/mysites/ /
+#	chown -R apache /var/www/mysites/
+#    EOH
+#end
+
+#directory "/var/www/mysites" do
+#    owner 'apache'
+#    recursive true
+#end
 
 service 'httpd' do
     action [:enable, :start]
+    subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
