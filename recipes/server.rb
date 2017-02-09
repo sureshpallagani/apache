fb4be2f07dcf260437387fae4ade706ba11fe49d
Cloud User <centos@ip-172-31-33-213.us-west-2.compute.internal>
2017-02-09 05:48:36 +0000

    Ohai

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,7 +9,10 @@ package 'httpd' do
 end
 
 file 'var/www/html/index.html' do
-    content '<h1>Hello, World!</h1>'
+    content "<h1>Hello, World!</h1>
+		<h2>IPADDRESS: #{node['ipaddress']}</h2>
+		<h2>HOSTNAME: #{node['hostname']}</h2>
+	"
 end
 
 service 'httpd' do
